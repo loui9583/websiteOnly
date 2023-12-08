fc823c7cf86284a1e7f324116acdc132c8e1cc7f
loui9583 <100029706+loui9583@users.noreply.github.com>
2023-12-08 23:37:17 +0100

    Initial commit

---
+++ src/main/java/com/example/websiteonly/WebsiteOnlyApplication.java
@@ -0,0 +1,15 @@
+package com.example.websiteonly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteOnlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteOnlyApplication.class, args);
+    }
+
+}
+
+//https://project-2-louis-nordsoe.azurewebsites.net/api
---
+++ src/test/java/com/example/websiteonly/WebsiteOnlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websiteonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteOnlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
